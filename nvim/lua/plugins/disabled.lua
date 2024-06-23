ebb0fc20273d7ad7655cc08db563dc708f2b3fab
SealCia1123 <maoduchai404@gmail.com>
2024-06-23 23:58:25 +0700

    update nvim

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,6 +1,5 @@
 return {
     -- disable trouble
     { "akinsho/bufferline.nvim", enabled = false },
-    -- { "kawre/neotab.nvim", enabled = false },
     { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
