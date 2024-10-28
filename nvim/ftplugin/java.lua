5bfa971daf5302e38f52c3084b61841a08595f28
SealCia1123 <138353079+SealCia1123@users.noreply.github.com>
2024-10-28 17:12:16 +0700

    Update java.lua

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -1,73 +1,26 @@
+local home = os.getenv("HOME")
+local workspace_path = home .. "/.local/share/nvim/jdtls-workspace/"
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+local workspace_dir = workspace_path .. project_name
+
 local status, jdtls = pcall(require, "jdtls")
 if not status then
     return
 end
-
-local function capabilities()
-    local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-    if status_ok then
-        return cmp_nvim_lsp.default_capabilities()
-    end
-
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities.textDocument.completion.completionItem.snippetSupport = true
-    capabilities.textDocument.completion.completionItem.resolveSupport = {
-        properties = {
-            "documentation",
-            "detail",
-            "additionalTextEdits",
-        },
-    }
-
-    return capabilities
-end
-
-local function directory_exists(path)
-    local f = io.popen("cd " .. path)
-    local ff = f:read("*all")
-
-    if ff:find("ItemNotFoundException") then
-        return false
-    else
-        return true
-    end
-end
-
-local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }
-local root_dir = require("jdtls.setup").find_root(root_markers)
-
--- calculate workspace dir
-local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
-local workspace_dir = vim.fn.stdpath("data") .. "/site/java/workspace-root/" .. project_name
-if directory_exists(workspace_dir) then
-else
-    os.execute("mkdir " .. workspace_dir)
-end
--- get the mason install path
-local install_path = require("mason-registry").get_package("jdtls"):get_install_path()
-
--- get the current OS
-local os
-if vim.fn.has("macunix") then
-    os = "mac"
-elseif vim.fn.has("win32") then
-    os = "win"
-else
-    os = "linux"
-end
-
-local bundles = {}
-local mason_path = vim.fn.glob(vim.fn.stdpath("data") .. "/mason/")
-vim.list_extend(bundles, vim.split(vim.fn.glob(mason_path .. "packages/java-test/extension/server/*.jar"), "\n"))
-vim.list_extend(
-    bundles,
-    vim.split(
-        vim.fn.glob(mason_path .. "packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar"),
-        "\n"
-    )
-)
-
+local extendedClientCapabilities = jdtls.extendedClientCapabilities
 local config = {
+    capabilities = {
+        workspace = {
+            configuration = true,
+        },
+        textDocument = {
+            completion = {
+                completionItem = {
+                    snippentSupport = true,
+                },
+            },
+        },
+    },
     cmd = {
         "java",
         "-Declipse.application=org.eclipse.jdt.ls.core.id1",
@@ -75,52 +28,50 @@ local config = {
         "-Declipse.product=org.eclipse.jdt.ls.core.product",
         "-Dlog.protocol=true",
         "-Dlog.level=ALL",
-        "-javaagent:" .. install_path .. "/lombok.jar",
-        "-Xms1g",
+        "-Xmx1g",
         "--add-modules=ALL-SYSTEM",
         "--add-opens",
         "java.base/java.util=ALL-UNNAMED",
         "--add-opens",
         "java.base/java.lang=ALL-UNNAMED",
+        "-javaagent:" .. home .. "/.local/share/nvim/mason/packages/jdtls/lombok.jar",
         "-jar",
-        vim.fn.glob(install_path .. "/plugins/org.eclipse.equinox.launcher_*.jar"),
+        vim.fn.glob(home .. "/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
         "-configuration",
-        install_path .. "/config_" .. os,
+        home .. "/.local/share/nvim/mason/packages/jdtls/config_linux",
         "-data",
         workspace_dir,
     },
-    capabilities = capabilities(),
-    root_dir = root_dir,
+    root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }),
+
     settings = {
-        java = {},
+        java = {
+            signatureHelp = { enabled = true },
+            extendedClientCapabilities = extendedClientCapabilities,
+            maven = {
+                downloadSources = true,
+            },
+            referencesCodeLens = {
+                enabled = true,
+            },
+            references = {
+                includeDecompiledSources = true,
+            },
+            inlayHints = {
+                parameterNames = {
+                    enabled = "all", -- literals, all, none
+                },
+            },
+            format = {
+                enabled = false,
+            },
+        },
     },
 
     init_options = {
-        bundles = {
-            vim.fn.glob(
-                mason_path .. "packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar",
-                "\n"
-            ),
-        },
+        bundles = {},
+        usePlaceholders = true,
     },
 }
-
-config["on_attach"] = function(client, bufnr)
-    local _, _ = pcall(vim.lsp.codelens.refresh)
-    require("jdtls.dap").setup_dap_main_class_configs()
-    jdtls.setup_dap({ hotcodereplace = "auto" })
-    require("user.lsp.handlers").on_attach(client, bufnr)
-end
-
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-    pattern = { "*.java" },
-    callback = function()
-        local _, _ = pcall(vim.lsp.codelens.refresh)
-    end,
-})
-
-jdtls.start_or_attach(config)
-
-vim.cmd(
-    [[command! -buffer -nargs=? -complete=custom,v:lua.require'jdtls'._complete_set_runtime JdtSetRuntime lua require('jdtls').set_runtime(<f-args>)]]
-)
+config.capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+require("jdtls").start_or_attach(config)
