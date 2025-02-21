dc820d02110a98c91bddc89770979e1729722215
SealCia1123 <maoduchai404@gmail.com>
2025-02-21 23:21:25 +0700

    update nvim

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -9,13 +9,24 @@ return {
     },
     config = function()
       require("telescope").setup({
+        defaults = {
+          layout_config = {
+            width = 0.9,
+            preview_width = 0.5,
+          },
+        },
         pickers = {
           find_files = {
-            find_command = { "rg", "--files", "--hidden", "-g", "!.git" },
+            hidden = true,
           },
         },
         extensions = {
-          fzf = {},
+          fzf = {
+            fuzzy = true, -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true, -- override the file sorter
+            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+          },
         },
       })
       require("telescope").load_extension("fzf")
